440ed9765d326744bc526b807a1bfca7e3efe50f
unknown <linjunfu@ucredit.com>
2019-07-30 14:38:08 +0800

    增加 index 首页

--- src/pages/bond/index/router/index.js
+++ src/pages/bond/index/router/index.js
@@ -4,26 +4,11 @@ import Router from 'vue-router'
 Vue.use(Router)
 export default new Router({
     routes: [
-        // {
-        //     name: 'cancel-email-hk',
-        //     path: '/cancel-email-hk',
-        //     meta: { title: '郵件退訂' },
-        //     component: () =>
-        //         import(`@/pages/template/email/views/cancel-email/hk.vue`)
-        // },
-        // {
-        //     name: 'cancel-email-ch',
-        //     path: '/cancel-email-ch',
-        //     meta: { title: '邮件退订' },
-        //     component: () =>
-        //         import(`@/pages/template/email/views/cancel-email/ch.vue`)
-        // },
-        // {
-        //     name: 'jumper',
-        //     path: '/jumper/:type',
-        //     meta: { title: 'loading...' },
-        //     component: () =>
-        //         import(`@/pages/template/email/views/jumper/index.vue`)
-        // }
+        {
+            name: 'index',
+            path: '/',
+            meta: { title: '债券首页' },
+            component: () => import(`@/pages/bond/index/views/index/index.vue`)
+        }
     ]
 })
---
+++ src/pages/bond/index/views/index/index.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+    .bond-index-wrapper
+        van-swipe.banner(:autoplay="3000" indicator-color="white")
+            van-swipe-item 1
+            van-swipe-item 2
+
+</template>
+<script>
+import { Swipe, SwipeItem } from 'vant'
+export default {
+    name: 'index',
+    data() {
+        return {}
+    },
+    components: {
+        [Swipe.name]: Swipe,
+        [SwipeItem.name]: SwipeItem
+    },
+    created() {}
+}
+</script>
+
+<style lang="scss" scoped>
+.bond-index-wrapper {
+    .banner {
+        width: 375px;
+        height: 150px;
+    }
+}
+</style>
