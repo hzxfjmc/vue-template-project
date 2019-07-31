fca30591e6e90f3dfc20de4ca55b92d6b69517de
unknown <linjunfu@ucredit.com>
2019-07-31 10:47:06 +0800

    接口函数增加注释

--- src/service/finance-server.js
+++ src/service/finance-server.js
@@ -2,26 +2,37 @@
 import httpRequest from '@/utils/http-request'
 const axios = new httpRequest()
 
-// 获取债券列表
+/**
+ * 获取债券列表
+ */
 export const getBondList = () => {
     return axios.post('/finance-server/api/get-bond-list/v1')
 }
 
-// 获取债券信息详情
+/**
+ * 获取债券信息详情
+ * @param {Number} id 债券id
+ */
 export const getBondDetail = id => {
     return axios.post('/finance-server/api/get-bond-detail/v1', {
         id
     })
 }
 
-// 获取债券价格详情
+/**
+ * 获取债券价格详情
+ * @param {Number} id 债券id
+ */
 export const getBondPrices = id => {
     return axios.post('/finance-server/api/get-bond-prices/v1', {
         id
     })
 }
 
-// 获取债券当前价格
+/**
+ * 获取债券当前价格
+ * @param {Number} id 债券id
+ */
 export const getCurrentBondPrices = id => {
     return axios.post('/finance-server/api/get-current-bond-prices/v1', {
         id
