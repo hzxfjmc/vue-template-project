ad17740ef1aa26f7d0310e111631cc89db71caee
unknown <linjunfu@ucredit.com>
2019-08-02 11:39:32 +0800

    购买流程模块

---
+++ src/pages/bond/index/views/bond-detail/components/purchasing-process/index.vue
@@ -0,0 +1,137 @@
+<template lang="pug">
+    .detail-purchasing-process-wrapper
+        .process-steps
+            .process-steps__items
+                .process-step
+                    .process-step__title 下单购买
+                    .process-step__circle-container
+                        i.process-step__circle
+                    .process-step__line
+                .process-step
+                    .process-step__title T+1日计息
+                    .process-step__circle-container
+                        i.process-step__circle
+                    .process-step__line
+                .process-step
+                    .process-step__title 3.22、9.22为付息日
+                    .process-step__circle-container
+                        span.process-step__text 付息日支付利息，持有中途可卖出
+                    .process-step__line
+                .process-step
+                    .process-step__title 到期退出
+                        .process-step__title-tips 2020.03.22
+                    .process-step__circle-container
+                        i.process-step__circle
+                    .process-step__line
+
+
+        //- van-steps
+        //-     van-step 买家下单
+        //-     van-step 买家下单
+        //-     van-step 买家下单
+        //-     van-step 买家下单
+</template>
+<script>
+import { Step, Steps } from 'vant'
+export default {
+    name: 'PurchasingProcess',
+    components: {
+        [Step.name]: Step,
+        [Steps.name]: Steps
+    }
+}
+</script>
+<style lang="scss" scoped>
+.process-steps {
+    // overflow: hidden;
+    padding: 0 10px 0 13px;
+    background-color: #fff;
+}
+.process-steps__items {
+    display: flex;
+    // overflow: hidden;
+    position: relative;
+    // margin: 0 0 0.2rem;
+}
+.process-step {
+    position: relative;
+    flex: 1;
+    padding: 23px 0 26px;
+    color: #969799;
+    font-size: 0.28rem;
+    &:first-child {
+        .process-step__title {
+            transform: none;
+            margin-left: 0;
+        }
+    }
+    &:nth-child(2) {
+        left: -26px;
+    }
+    &:nth-child(3) {
+        left: -8px;
+    }
+    &:last-child {
+        position: absolute;
+        right: 0;
+        width: auto;
+        .process-step__title {
+            transform: none;
+            margin-left: 0;
+            text-align: right;
+        }
+        .process-step__circle-container {
+            left: auto;
+            right: 3px;
+        }
+    }
+    .process-step__title {
+        position: relative;
+        display: inline-block;
+        margin-left: 3px;
+        font-size: 0.24rem;
+        line-height: 20px;
+        transform: translate3d(-50%, 0, 0);
+        .process-step__title-tips {
+            position: absolute;
+            right: 0;
+            font-size: 0.2rem;
+            line-height: 1;
+        }
+    }
+    .process-step__circle-container {
+        position: absolute;
+        top: 6px;
+        left: 0;
+        // background-color: #fff;
+        z-index: 1;
+        .process-step__circle {
+            display: block;
+            width: 6px;
+            height: 6px;
+            background: #393939;
+            border-radius: 50%;
+        }
+        .process-step__text {
+            position: relative;
+            top: 3px;
+            display: block;
+            padding: 2px 4px 3px;
+            background: rgba(235, 235, 235, 1);
+            color: #393939;
+            font-size: 0.2rem;
+            line-height: 14px;
+            white-space: nowrap;
+            transform: translate3d(-50%, -50%, 0);
+        }
+    }
+    .process-step__line {
+        position: absolute;
+        top: 8px;
+        left: 0;
+        width: 100%;
+        height: 2px;
+        background-color: #ebebeb;
+    }
+}
+</style>
--- src/pages/bond/index/views/bond-detail/index.vue
+++ src/pages/bond/index/views/bond-detail/index.vue
@@ -1,17 +1,22 @@
 <template lang="pug">
     .bond-detail-wrapper
         detail-header
+        van-panel(title="购买流程")
+            purchasing-process
 </template>
 <script>
 // import { Swipe, SwipeItem } from 'vant'
 // import { getBondList } from '@/service/finance-server.js'
+import { Panel } from 'vant'
 import DetailHeader from './components/detail-header/index.vue'
+import PurchasingProcess from './components/purchasing-process/index.vue'
 export default {
     name: 'BondList',
     components: {
-        // [Swipe.name]: Swipe,
+        [Panel.name]: Panel,
         // [SwipeItem.name]: SwipeItem,
-        DetailHeader
+        DetailHeader,
+        PurchasingProcess
     },
     async created() {},
     data() {
@@ -24,5 +29,21 @@ export default {
 <style lang="scss" scoped>
 .bond-detail-wrapper {
     padding: 10px;
+}
+// 微调 vant panel 样式
+.van-panel {
+    margin-top: 10px;
+    border-radius: 4px;
+    .van-panel__header {
+        padding: 14px;
+        &:after {
+            display: none;
+        }
+    }
+    .van-cell__title {
+        color: #393939;
+        font-size: 0.28rem;
+        line-height: 20px;
+    }
 }
 </style>
