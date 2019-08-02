deaed6967b99c697362b7688b9aeca5f343ab701
unknown <linjunfu@ucredit.com>
2019-08-02 19:58:49 +0800

    提取公用的 fix 操作按钮

---
+++ src/pages/bond/index/biz-components/fix-operate-button/index.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+    .fix-operate-btn(:disabled="disabled") {{ text }}
+</template>
+
+<script>
+export default {
+    name: 'FixedOperateBtn',
+    props: {
+        disabled: {
+            type: Boolean,
+            default: false
+        },
+        text: {
+            type: String,
+            default: ''
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.fix-operate-btn {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 48px;
+    color: #fff;
+    font-size: 0.32rem;
+    text-align: center;
+    line-height: 48px;
+    background-color: #0d50d8;
+    &[disabled='disabled'] {
+        background-color: #dadada;
+    }
+}
+</style>
