792bd144fa855f44786dd76f7e207970678ec5ac
unknown <linjunfu@ucredit.com>
2019-08-05 17:35:49 +0800

    删除无用 service 内容

--- src/service/news-helpcenter.js
+++
@@ -1,8 +0,0 @@
-import httpRequest from '@/utils/http-request'
-import { API_BASE_URL } from '@/utils/DOMAIN.js'
-const axios = new httpRequest(API_BASE_URL.HZ)
-
-// 帮助中心
-export const queryMessageDetail = params => {
-    return axios.get(`/news-helpcenter/api/v1/query/messagedetail`, params)
-}
