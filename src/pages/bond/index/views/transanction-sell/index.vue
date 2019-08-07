c4e61600130c9c11ce70c6b13b3612f1c6a906d2
unknown <linjunfu@ucredit.com>
2019-08-07 18:24:33 +0800

    买卖模块修改

--- src/pages/bond/index/views/transanction-sell/index.vue
+++ src/pages/bond/index/views/transanction-sell/index.vue
@@ -1,17 +1,14 @@
 <template lang="pug">
     .transaction-wrapper
-        transacntion-card
-        fixed-operate-btn(text="确认卖出")
+        transacntion-card(btnText="确认卖出")
 </template>
 
 <script>
-import FixedOperateBtn from '@/pages/bond/index/biz-components/fix-operate-button/index.vue'
 import TransacntionCard from '@/pages/bond/index/biz-components/transaction-card/index.vue'
 export default {
     name: 'TransanctionBuy',
     components: {
-        TransacntionCard,
-        FixedOperateBtn
+        TransacntionCard
     },
     data() {
         return {}
@@ -22,17 +19,5 @@ export default {
 <style lang="scss" scoped>
 .transaction-wrapper {
     padding: 10px 10px 48px;
-    .operate-btn {
-        position: fixed;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        height: 48px;
-        background-color: #ffbf32;
-        color: #fff;
-        font-size: 16px;
-        text-align: center;
-        line-height: 48px;
-    }
 }
 </style>
