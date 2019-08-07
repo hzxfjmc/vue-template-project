164094c15daca8ea5d89570d9bc8e7ed45382561
unknown <linjunfu@ucredit.com>
2019-08-07 13:48:25 +0800

    get改为getForm

--- src/service/user-server.js
+++ src/service/user-server.js
@@ -31,12 +31,16 @@ export const riskAssessAnswer = data => {
  * 获取当前用户风险测评结果
  */
 export const riskAssessResult = () => {
-    return axios.get('/user-server/api/risk-assess-result/v1')
+    return axios.getForm('/user-server/api/risk-assess-result/v1')
 }
 
 /**
  * 获取风险测评题目
  */
 export const riskAssessSubject = () => {
-    return axios.get('/user-server/api/risk-assess-subject/v1')
+    return axios.getForm('/user-server/api/risk-assess-subject/v1')
+}
+
+export function jsAuth() {
+    return axios.getForm(`/user-server/web/js-auth/v1`)
 }
