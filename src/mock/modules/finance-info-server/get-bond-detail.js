1275c673b816eea0c49a829c4b6dcdab261fd474
unknown <linjunfu@ucredit.com>
2019-08-14 14:13:38 +0800

    更新mock

--- src/mock/modules/finance-info-server/get-bond-detail.js
+++ src/mock/modules/finance-info-server/get-bond-detail.js
@@ -91,7 +91,7 @@ const formatData = formatMockData({
         sellYtm: /\d{1}\.\d{2}%/,
         'updateTime|1564577990837-2564577990837': 0
     },
-    'prices|1-3': [
+    'prices|200-300': [
         {
             'buyPrice|1-100.3': 1,
             buyYtm: /\d{1}\.\d{2}%/,
