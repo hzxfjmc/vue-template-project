255d40fcc6d92feff2e305eacb9fbb06a2736250
zenghong <809343228@qq.com>
2019-08-28 17:47:15 +0800

    feat： 复用债券风险测评结果

--- src/pages/funds/index/router/index.js
+++ src/pages/funds/index/router/index.js
@@ -13,6 +13,22 @@ export default new Router({
             meta: { title: '基金详情' },
             component: () =>
                 import(`@/pages/funds/index/views/funds-details/index.vue`)
+        },
+        {
+            name: 'risk-assessment',
+            path: '/risk-assessment',
+            meta: { title: '基金详情' },
+            component: () =>
+                import(`@/pages/funds/index/views/risk-assessment/index.vue`)
+        },
+        {
+            name: 'risk-assessment-result',
+            path: '/risk-assessment-result',
+            meta: { title: '测评结果' },
+            component: () =>
+                import(
+                    `@/pages/funds/index/views/risk-assessment-result/index.vue`
+                )
         }
     ]
 })
