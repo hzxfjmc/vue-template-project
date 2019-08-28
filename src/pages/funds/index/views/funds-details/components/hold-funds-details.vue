bde6f4e8753be7d9206935704c41b72910d92403
azu <huangzuxing@youxin.com>
2019-08-28 19:27:41 +0800

    基金详情页面

--- src/pages/funds/index/views/funds-details/components/hold-funds-details.vue
+++ src/pages/funds/index/views/funds-details/components/hold-funds-details.vue
@@ -1,3 +1,37 @@
-<template lang="">
-    <div></div>
+<template lang="pug">
+.hold-funds-details
+    span 持仓详情
+    van-row()
+        van-col( span="8" v-for="item of list" class="funds-row" :class="item.layout") 
+            span.holdSubtitle {{item.label}}
+            p.holdNumber {{item.value}}
 </template>
+<script>
+import { Row, Col } from 'vant'
+import { holdDetailsData } from './hold-funds-data'
+export default {
+    components: {
+        Row,
+        Col
+    },
+    data() {
+        return {
+            list: holdDetailsData
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+.hold-funds-details {
+    margin: 10px 0 0 0;
+    background: $background-color;
+    padding: 10px;
+    height: 160px;
+    .funds-row {
+        margin: 10px 0 0 0;
+        .holdNumber {
+            font-size: 18px;
+        }
+    }
+}
+</style>
--- src/pages/funds/index/views/funds-details/index.vue
+++ src/pages/funds/index/views/funds-details/index.vue
@@ -3,18 +3,30 @@
     FundsDetailsHeader
     
     FundsDetailsEchart
+
+    HoldFundsDetails
+
+    FundsDetailsList 
 </template>
 <script>
 import FundsDetailsHeader from './components/funds-details-header'
 import FundsDetailsEchart from './components/funds-details-echart'
+import HoldFundsDetails from './components/hold-funds-details'
+import FundsDetailsList from './components/funds-details-list'
 export default {
     components: {
         FundsDetailsHeader,
-        FundsDetailsEchart
+        FundsDetailsEchart,
+        HoldFundsDetails,
+        FundsDetailsList
     },
     data() {
         return {}
     }
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.funds-details {
+    overflow: hidden;
+}
+</style>
