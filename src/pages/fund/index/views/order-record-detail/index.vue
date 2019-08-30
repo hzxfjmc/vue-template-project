7570ca5b6b2d58b30c91ad27f662e67a66578126
zenghong <809343228@qq.com>
2019-08-30 18:51:08 +0800

    订单详情

--- src/pages/fund/index/views/order-record-detail/index.vue
+++ src/pages/fund/index/views/order-record-detail/index.vue
@@ -2,18 +2,45 @@
     .order-record-detail-container
         .fund-introduce
             .fund-name {{fundName}}
-            .fund-detail
-                .fund-detail-item(v-for="(item) in fundDetailList") {{item}}
-        
+            .fund-detail {{fundDetail}}
+        order-status-about
+        van-cell-group(class="order-group")
+            van-cell(class="order-time" )
+                .order-item.flex(v-for="item in orderAboutList")
+                    span.itemName {{item.name}}
+                    span {{item.value}}
+            van-cell(class="order-money-cell" )
+                .order-money.flex
+                    .left-title.flex
+                        span.type {{orderType}}
+                        span.type-text 金额
+                    .right-value {{moneyNum}}
+        .btn-buy-more
+            van-button(type="info" round  size="large" @click="buyMoreHandle") 再买一笔
+    
 </template>
 
 <script>
-import riskAssessmentMixin from '@/mixins/bond/risk-assessment/index.js'
+import orderStatusAbout from './components/order-status-about'
 export default {
-    mixins: [riskAssessmentMixin],
+    components: {
+        orderStatusAbout
+    },
     data() {
         return {
-            fundName: 'Pimco 亚洲投资级债券基金-A2'
+            fundName: 'Pimco 亚洲投资级债券基金-A2',
+            fundDetail: 'ISIN:IE00B0MD9M11',
+            orderAboutList: [
+                { name: '订单生成时间', value: '2019-07-12 15:06:44' },
+                { name: '订单号', value: '01907120540425132220050' }
+            ],
+            orderType: '赎回',
+            moneyNum: '2,000.000.00'
+        }
+    },
+    methods: {
+        buyMoreHandle() {
+            this.$router.push({ name: 'index' })
         }
     }
 }
@@ -21,8 +48,61 @@ export default {
 
 <style lang="scss" scoped>
 .order-record-detail-container {
+    height: 100%;
+    background-color: $background-color;
     .fund-introduce {
-        height: 75px;
+        background-color: $background-color;
+        &::after {
+            width: 100%;
+            content: '';
+            display: block;
+            height: 10px;
+            background-color: $background-bottom-color;
+        }
+        .fund-name {
+            font-size: 16px;
+            line-height: 22px;
+            padding-top: 15px;
+            margin-bottom: 6px;
+            padding-left: 12px;
+        }
+        .fund-detail {
+            font-size: 14px;
+            color: $text-color5;
+            padding: 0 0 12px 12px;
+        }
+    }
+    .order-group {
+        padding-bottom: 41px;
+        .order-time {
+            .itemName {
+                color: $text-color5;
+                margin-bottom: 10px;
+            }
+        }
+        .order-money-cell {
+            border-bottom: 1px solid rgba($color: $text-color, $alpha: 0.05);
+            .order-money {
+                .left-title {
+                    flex-direction: column;
+                    .type {
+                        font-size: 16px;
+                        line-height: 22px;
+                    }
+                    .type-text {
+                        @extend .type;
+                        color: $text-color5;
+                    }
+                }
+                .right-value {
+                    font-size: 24px;
+                    line-height: 44px;
+                }
+            }
+        }
+    }
+    .btn-buy-more {
+        padding: 0 16px;
     }
 }
 </style>
