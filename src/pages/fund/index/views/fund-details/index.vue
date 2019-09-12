9e0a4685ce7ed215e592ab09dbaa55bab250a8a2
azu <huangzuxing@youxin.com>
2019-09-12 18:57:43 +0800

    增加交易记录传值

--- src/pages/fund/index/views/fund-details/index.vue
+++ src/pages/fund/index/views/fund-details/index.vue
@@ -17,6 +17,7 @@
             :positionStatus = "positionStatus"
             :fondCode = "fondCode"
             :scroll = "scroll"
+            :fundHeaderInfoVO = "fundHeaderInfoVO" 
             :fundOverviewInfoVO="fundOverviewInfoVO") 
     .fund-footer-content(v-if="btnShow")
         van-button(class="btn fund-check" @click="toRouter('/fund-redemption')") {{$t('redeem')}}
