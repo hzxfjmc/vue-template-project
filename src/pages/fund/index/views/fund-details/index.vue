27e21ffee16fcb01c30697108ea228cf1463fdcd
azu <huangzuxing@youxin.com>
2019-09-18 21:00:46 +0800

    二进制转换

--- src/pages/fund/index/views/fund-details/index.vue
+++ src/pages/fund/index/views/fund-details/index.vue
@@ -242,6 +242,7 @@ export default {
                     }
                 })
             } else {
+                console.log(23)
                 let data = {
                     query: {
                         id: this.$route.query.id,
@@ -250,14 +251,10 @@ export default {
                         fundCode: this.fundCode
                     }
                 }
-                let arr = this.userInfo.extendStatusBit.toString(2).split('')
-                var step = 0
-                for (let i in arr) {
-                    if (arr[i] == 0) {
-                        step = i
-                    }
-                }
-                data.path = step < 4 ? '/open-permissions' : '/fund-subscribe'
+                data.path =
+                    (31 & this.userInfo.extendStatusBit) > 0
+                        ? '/fund-subscribe'
+                        : '/open-permissions'
                 this.$router.push(data)
             }
         },
