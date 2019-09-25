d12b07bee74adb29aa4f2e8bcc626ce578cc8b28
zenghong <809343228@qq.com>
2019-09-25 23:53:50 +0800

    fix:bug

--- src/mixins/bond/risk-assessment/index.js
+++ src/mixins/bond/risk-assessment/index.js
@@ -25,12 +25,6 @@ export default {
             canSubmit: false
         }
     },
-    // beforeRouteEnter(to, from, next) {
-    //     next(vm => {
-    //         console.log(vm, '001')
-    //         vm.judge()
-    //     })
-    // },
     computed: {
         titleI18n() {
             return {
@@ -72,9 +66,6 @@ export default {
         }
     },
     async created() {
-        if (!this.$route.query.id) {
-            await this.getCurrentUser()
-        }
         // 拉取测评题目
         try {
             let { subject, version } = await riskAssessSubject()
@@ -100,8 +91,9 @@ export default {
     methods: {
         // 提交测试题目
         async handleSubmit(action) {
+            if (!this.submitBtnDisabled && this.canSubmit) return
+            this.canSubmit = true
             try {
-                if (this.submitBtnDisabled && this.canSubmit) return
                 // 构造提交数据
                 let serializeData = this.subject.map(subjectItem => {
                     if (subjectItem.subject) {
@@ -130,21 +122,21 @@ export default {
                     assessOptionParams: serializeData,
                     subjectVersion: this.version
                 })
-                this.canSubmit = true
                 // 点击提交按钮时候，才进行跳转
                 if (action === 'submit') {
                     // 拉取风险测评结果
                     let res = await riskAssessResult()
-                    this.canSubmit = true
                     this.assessDefinition = res.assessDefinition
                     if (res.damagedStatus === 1) {
                         this.showEasyCustomer = true
+                        console.log(this.showEasyCustomer)
                     } else {
                         this.jumpToResult()
                     }
                 }
                 console.log(assessResult)
             } catch (e) {
+                this.canSubmit = false
                 console.log('riskAssessAnswer:error:>>>', e)
             }
         },
@@ -159,7 +151,6 @@ export default {
                     !this.$route.query.notFirstSubmit
                 ) {
                     console.log(this.userInfo.assessResult)
-
                     window.location.replace(
                         location.origin +
                             '/wealth/fund/index.html#/risk-assessment-result'
