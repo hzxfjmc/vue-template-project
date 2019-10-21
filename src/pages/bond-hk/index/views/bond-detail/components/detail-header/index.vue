e7cc8609913249a1d95ccb129ac4f3a5a119fcac
unknown <linjunfu@ucredit.com>
2019-10-21 10:08:22 +0800

    债券详情头部

--- src/pages/bond-hk/index/views/bond-detail/components/detail-header/index.vue
+++ src/pages/bond-hk/index/views/bond-detail/components/detail-header/index.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
     .detail-header
-        media-box(
+        media-box.bond-info(
             :title="headerTitle"
             :desc="headerDesc"
         )
@@ -14,25 +14,85 @@
             media-box.medal(
                 :thumb="featureThumbMedal"
                 title="安全性高xxxxxxx"
-                desc="苹果公司发行，公司稳健，违约风险低"
+                desc="蘋果公司發行，公司穩健，違約風險低"
             )
             media-box.money(
                 :thumb="featureThumbMoney"
-                title="起购金额低xxxxxxx"
-                desc="2000美元可购买xxxxxxxxxx"
+                title="起購金額低xxxxxxx"
+                desc="2000美元可購買xxxxxx"
             )
-
 </template>
 <script>
 import detailHeaderMixin from '@/mixins/bond/bond-detail/detail-header.js'
+import { calcCountDownDay } from '@/utils/tools.js'
 export default {
-    mixins: [detailHeaderMixin]
+    mixins: [detailHeaderMixin],
+    data() {
+        return {
+            featureThumbMedal: require('@/assets/img/bond/icon-medal.png'),
+            featureThumbMoney: require('@/assets/img/bond/icon-money.png'),
+            titleStyle: {
+                color: '#fff',
+                'font-size': '0.4rem',
+                'line-height': '0.4rem'
+            },
+            descStyle: {
+                'margin-top': '0.12rem',
+                color: 'rgba(255, 255, 255, 0.6)',
+                'font-size': '0.24rem',
+                'line-height': '0.4rem'
+            }
+        }
+    },
+    computed: {
+        colData() {
+            let _this = this
+            let obj = [
+                {
+                    title:
+                        (this.currentPrice &&
+                            this.currentPrice &&
+                            this.currentPrice.buyYtm &&
+                            (this.currentPrice.buyYtm - 0).toFixed(3) + '%') ||
+                        '--',
+                    desc: '到期年化收益率',
+                    click: () => {
+                        this.$dialog.alert({
+                            message: `到期收益率指按买入价格买入债券并持有到期，获得的全部利息和本金计算而来的年平均收益率。\n
+到期收益率综合考虑了购买价格、持有期限、票面利率等因素，是非常重要的参考要素。\n
+注：展示数值为已加入预估佣金、平台使用费之后的到期收益率。`,
+                            messageAlign: 'left',
+                            confirmButtonText: '我知道了'
+                        })
+                    },
+                    class: 'icon-wenhao'
+                },
+                {
+                    title: calcCountDownDay(
+                        _this.bondUneditableInfo &&
+                            _this.bondUneditableInfo.dueTime
+                    ),
+                    desc: '剩餘限期'
+                },
+                {
+                    title:
+                        (this.bondUneditableInfo &&
+                            this.bondUneditableInfo.enumPaymentFrequency &&
+                            this.bondUneditableInfo.enumPaymentFrequency
+                                .name) ||
+                        '--',
+                    desc: '付息頻率'
+                }
+            ]
+            return obj
+        }
+    }
 }
 </script>
 <style lang="scss" scoped>
 .detail-header {
-    background-color: #2f79ff;
-    border-radius: 4px;
+    background-color: rgba($color: #0055ff, $alpha: 0.6);
+    border-radius: 10px;
     .header-column {
         padding: 0 10px 14px 14px;
         >>> .van-col {
@@ -43,8 +103,25 @@ export default {
 </style>
 <style lang="scss">
 // 微调 media-box 样式
+.detail-header {
+    .media-box.bond-info {
+        padding: 20px 14px 18px;
+        .media-box__content {
+            .media-box__title {
+                font-size: 0.5rem;
+                font-weight: 500;
+                line-height: 36px;
+            }
+            .media-box__desc {
+                margin-top: 1px;
+                color: rgba($color: #fff, $alpha: 0.6);
+                font-size: 0.28rem;
+                line-height: 17px;
+            }
+        }
+    }
+}
 .header-features {
-    background: rgba(0, 0, 0, 0.06);
     padding: 13px 0;
     .media-box {
         padding: 0;
--- src/pages/bond/index/views/bond-detail/components/detail-header/index.vue
+++ src/pages/bond/index/views/bond-detail/components/detail-header/index.vue
@@ -25,8 +25,75 @@
 </template>
 <script>
 import detailHeaderMixin from '@/mixins/bond/bond-detail/detail-header.js'
+import { calcCountDownDay } from '@/utils/tools.js'
 export default {
-    mixins: [detailHeaderMixin]
+    mixins: [detailHeaderMixin],
+    data() {
+        return {
+            featureThumbMedal: require('@/assets/img/bond/icon-medal.png'),
+            featureThumbMoney: require('@/assets/img/bond/icon-money.png'),
+            titleStyle: {
+                color: '#fff',
+                'font-size': '0.4rem',
+                'line-height': '0.52rem'
+            },
+            descStyle: {
+                'margin-top': '0.04rem',
+                color: 'rgba(255, 255, 255, 0.6)',
+                'font-size': '0.24rem',
+                'line-height': '0.34rem'
+            }
+        }
+    },
+    computed: {
+        colData() {
+            let _this = this
+            let obj = [
+                {
+                    title:
+                        (this.currentPrice &&
+                            this.currentPrice &&
+                            this.currentPrice.buyYtm &&
+                            (this.currentPrice.buyYtm - 0).toFixed(3) + '%') ||
+                        '--',
+                    desc: '到期年化收益率',
+                    click: () => {
+                        this.$dialog.alert({
+                            message: `到期收益率指按买入价格买入债券并持有到期，获得的全部利息和本金计算而来的年平均收益率。\n
+到期收益率综合考虑了购买价格、持有期限、票面利率等因素，是非常重要的参考要素。\n
+注：展示数值为已加入预估佣金、平台使用费之后的到期收益率。`,
+                            messageAlign: 'left',
+                            confirmButtonText: '我知道了'
+                        })
+                    }
+                },
+                {
+                    title: calcCountDownDay(
+                        _this.bondUneditableInfo &&
+                            _this.bondUneditableInfo.dueTime
+                    ),
+                    desc: '剩余期限'
+                },
+                {
+                    title:
+                        (this.bondUneditableInfo &&
+                            this.bondUneditableInfo.minFaceValue &&
+                            (this.bondUneditableInfo.minFaceValue - 0).toFixed(
+                                3
+                            )) ||
+                        '--',
+                    desc:
+                        '起购金额(' +
+                        ((this.bondUneditableInfo &&
+                            this.bondUneditableInfo.enumCurrency &&
+                            this.bondUneditableInfo.enumCurrency.name) ||
+                            '美元') +
+                        '/份)'
+                }
+            ]
+            return obj
+        }
+    }
 }
 </script>
 <style lang="scss" scoped>
