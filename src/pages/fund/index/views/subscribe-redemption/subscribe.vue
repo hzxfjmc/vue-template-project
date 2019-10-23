e14f828969f9a61cc12c637b74061acdd0a7e687
linjiajun <linjiajun@youxin.com>
2019-10-23 16:50:02 +0800

    feat: 基金优化需求 购买页添加协议弹窗 边框修改

--- src/pages/fund/index/views/subscribe-redemption/subscribe.vue
+++ src/pages/fund/index/views/subscribe-redemption/subscribe.vue
@@ -14,7 +14,7 @@
                         .right-item 
                             .right-item-subscriptionFee(v-if="index=='subscriptionFee'")
                                 span {{item.value}}%
-                            .right-item-buyMoney(v-else-if="index=='buyMonny'")
+                            .right-item-buyMoney.border-bottom(v-else-if="index=='buyMonny'")
                                 input(v-model="item.value" type="number")
                             .right-item-other(v-else)
                                 span {{item.value}}
