3d1b9ded81300f988e90dd44df51849c91851935
unknown <linjunfu@ucredit.com>
2019-11-06 15:31:17 +0800

    路由标题修改

--- src/pages/bond/index/router/index-hk.js
+++ src/pages/bond/index/router/index-hk.js
@@ -4,48 +4,48 @@ let routerList = [
     {
         name: 'index',
         path: hkpath,
-        meta: { title: '债券' },
+        meta: { title: '債券交易' },
         component: () => import(`@/pages/bond/index/views/index/index-hk.vue`)
     },
     {
         name: 'bond-detail',
         path: '/bond-detail',
-        meta: { title: '债券详情' },
+        meta: { title: '債券详情' },
         component: () =>
             import(`@/pages/bond/index/views/bond-detail/index-hk.vue`)
     },
     {
         name: 'transaction-sell',
         path: '/transaction-sell',
-        meta: { title: '卖出' },
+        meta: { title: '賣出' },
         component: () =>
             import(`@/pages/bond/index/views/transaction-sell/index-hk.vue`)
     },
     {
         name: 'transaction-buy',
         path: '/transaction-buy',
-        meta: { title: '买入' },
+        meta: { title: '買入' },
         component: () =>
             import(`@/pages/bond/index/views/transaction-buy/index-hk.vue`)
     },
     {
         name: 'risk-warning',
         path: '/risk-warning',
-        meta: { title: '风险提示' },
+        meta: { title: '風險提示' },
         component: () =>
             import(`@/pages/bond/index/views/risk-warning/index-hk.vue`)
     },
     {
         name: 'risk-assessment',
         path: '/risk-assessment',
-        meta: { title: '风险测评' },
+        meta: { title: '風險測評' },
         component: () =>
             import(`@/pages/bond/index/views/risk-assessment/index-hk.vue`)
     },
     {
         name: 'risk-appropriate-result',
         path: '/risk-appropriate-result',
-        meta: { title: '产品匹配' },
+        meta: { title: '產品匹配' },
         component: () =>
             import(
                 `@/pages/bond/index/views/risk-appropriate-result/index-hk.vue`
@@ -54,7 +54,7 @@ let routerList = [
     {
         name: 'risk-assessment-result',
         path: '/risk-assessment-result',
-        meta: { title: '测评结果' },
+        meta: { title: '測評結果' },
         component: () =>
             import(
                 `@/pages/bond/index/views/risk-assessment-result/index-hk.vue`
