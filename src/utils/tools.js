12166d07d452b71f8b33510c99942be924fa52f3
linjiajun <linjiajun@youxin.com>
2019-11-13 15:30:10 +0800

    feat: 基金优化需求 千分位修改 --build -d fund

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -183,6 +183,7 @@ export function transNumToThousandMark(num = '0', dot = 2) {
         })
         return docBeforeNum + '.' + numArr[1].substring(0, dot)
     } else {
+        num = Number(num).toFixed(dot)
         let number = num.replace(/\d{1,3}(?=(\d{3})+$)/g, v1 => {
             return v1 + ','
         })
