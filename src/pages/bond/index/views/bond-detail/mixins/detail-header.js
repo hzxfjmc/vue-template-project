898a77050e94df52e6a86cb6c612a0de03c7f4ef
linjunfu <linjunfu@youxin.com>
2019-11-21 17:07:47 +0800

    fix ios 跳转pdf

--- src/pages/bond/index/views/bond-detail/mixins/detail-header.js
+++ src/pages/bond/index/views/bond-detail/mixins/detail-header.js
@@ -31,6 +31,10 @@ export default {
         }
     },
     computed: {
+        // 跳转pdf ios 部分使用原生 href 跳转不了
+        jumpPdf(url) {
+            window.location.href = url
+        },
         // 发行人
         headerTitle() {
             return (
