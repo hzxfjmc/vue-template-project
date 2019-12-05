a869f11618da5c2357c837b81806615d4e9ef116
linjiajun <linjiajun@youxin.com>
2019-12-05 16:33:13 +0800

    feat: 高腾基金灰度支持 修改ISIN码 --build -d fund

--- src/pages/fund/index/views/fund-details/index.vue
+++ src/pages/fund/index/views/fund-details/index.vue
@@ -98,7 +98,7 @@ export default {
             if (!this.isLogin) {
                 return true
             } else {
-                if (this.fundHeaderInfoVO.isin === 'HK0000447968') {
+                if (this.fundHeaderInfoVO.isin === 'HK0000478930') {
                     return (
                         this.userInfo &&
                         (this.userInfo.grayStatusBit & (1 << 3)) === 8
