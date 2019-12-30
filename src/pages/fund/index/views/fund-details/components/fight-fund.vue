61e59f546518cfcd6c2557e28cc77c120875fa48
azu <hzxfjmc@163.com>
2019-12-30 17:27:55 +0800

    拼团

---
+++ src/pages/fund/index/views/fund-details/components/fight-fund.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+.block__fight--fund
+    .blocl__fight--fund
+        .block__left
+            span 参与拼团
+            em.iconfont
+        .block__right
+            span 详细规则
+            em.iconfont
+    
+</template>
+<script>
+export default {
+    data() {
+        return {}
+    }
+}
+</script>
+<style lang="scss" scoped></style>
