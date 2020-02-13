e8a1b5152eac4c4aaf75d637f38cc84db9c21be9
azu <hzxfjmc@163.com>
2020-02-13 16:33:51 +0800

    change word

--- src/pages/fund/index/views/fund-order-list/index.vue
+++ src/pages/fund/index/views/fund-order-list/index.vue
@@ -253,7 +253,7 @@ export default {
                 })
                 await jsBridge.callApp('command_share', {
                     shareType: shareType,
-                    title: `我正在申购${this.fundName}，老司机开团，就差你上车啦！`,
+                    title: `我已申购${this.fundName}，老司机开团，就差你上车啦！`,
                     description:
                         '和我一起拼团买，尊享申购费折扣返还！点击了解详情>>>',
                     pageUrl: `${window.location.origin}/${shortPageUrl.url}`,
