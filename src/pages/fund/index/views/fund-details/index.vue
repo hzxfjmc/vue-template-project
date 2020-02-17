4ec5e182e5bddff9a3753fb7fc4add24e9a12216
azu <hzxfjmc@163.com>
2020-02-17 16:08:17 +0800

    修复分享

--- src/pages/fund/index/views/fund-details/index.vue
+++ src/pages/fund/index/views/fund-details/index.vue
@@ -461,7 +461,9 @@ export default {
     },
     methods: {
         handleShare() {
-            scheme.gotoWebview(`${window.location.href}`)
+            scheme.gotoWebview(
+                `${window.location.origin}/wealth/fund/index.html#/fund-details?id=${this.$route.query.id}`
+            )
         },
         async addGroupFollow() {
             try {
