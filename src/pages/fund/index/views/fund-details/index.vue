cfd87493db9745f8377524d148543712e26bde82
azu <hzxfjmc@163.com>
2020-02-17 10:14:18 +0800

    分享

--- src/pages/fund/index/views/fund-details/index.vue
+++ src/pages/fund/index/views/fund-details/index.vue
@@ -1112,10 +1112,42 @@ export default {
                 shareType: 'more',
                 title: `${this.fundHeaderInfoVO.fundName} ${this.fundHeaderInfoVO.isin}`,
                 description: this.$t([
-                    `我正在看一只基金，也推荐给你${this.fundHeaderInfoVO.fundName}（${this.fundHeaderInfoVO.isin}）近一年表现+26.55%，基金规模：USD 105亿
+                    `我正在看一只基金，也推荐给你${
+                        this.fundHeaderInfoVO.fundName
+                    }（${this.fundHeaderInfoVO.isin}）近一年表现${
+                        this.fundHeaderInfoVO.apy > 0
+                            ? '+' + this.fundHeaderInfoVO.apy
+                            : this.fundHeaderInfoVO.apy
+                    }%，基金规模：${this.undOverviewInfoVO.currency.name} ${(
+                        this.fundOverviewInfoVO.fundSize / 1000000000
+                    ).toFixed(2)}亿
                     https:XXXXXXXXXXXXXXXXXX.com（分享来自@友信智投客户端，立即下载，投资港/美/A股）`,
-                    `我睇緊一隻基金，你都可以睇下${this.fundHeaderInfoVO.fundName}（${this.fundHeaderInfoVO.isin}）近一年表現+26.55%，基金規模：USD 105億https:XXXXXXXXXXXXXXXXXX.com（分享來自@友信智投客戶端，立即下載，投資港/美/A股）`,
-                    `Fund I want to share with you ${this.fundHeaderInfoVO.fundName}（${this.fundHeaderInfoVO.isin}）1 Year +26.55%, AUM：USD 10.5Bhttps:XXXXXXXXXXXXXXXXXX.com（Shared From uSMART, Download to trade both HK, US & CN stock）`
+                    `我睇緊一隻基金，你都可以睇下${
+                        this.fundHeaderInfoVO.fundName
+                    }（${this.fundHeaderInfoVO.isin}）近一年表現${
+                        this.fundHeaderInfoVO.fundName
+                    }（${this.fundHeaderInfoVO.isin}）近一年表现${
+                        this.fundHeaderInfoVO.apy > 0
+                            ? '+' + this.fundHeaderInfoVO.apy
+                            : this.fundHeaderInfoVO.apy
+                    }%，基金規模：${this.undOverviewInfoVO.currency.name} ${(
+                        this.fundOverviewInfoVO.fundSize / 1000000000
+                    ).toFixed(
+                        2
+                    )}億https:XXXXXXXXXXXXXXXXXX.com（分享來自@友信智投客戶端，立即下載，投資港/美/A股）`,
+                    `Fund I want to share with you ${
+                        this.fundHeaderInfoVO.fundName
+                    }（${this.fundHeaderInfoVO.isin}）1 Year ${
+                        this.fundHeaderInfoVO.fundName
+                    }（${this.fundHeaderInfoVO.isin}）近一年表现${
+                        this.fundHeaderInfoVO.apy > 0
+                            ? '+' + this.fundHeaderInfoVO.apy
+                            : this.fundHeaderInfoVO.apy
+                    }%, AUM：${this.undOverviewInfoVO.currency.name} ${(
+                        this.fundOverviewInfoVO.fundSize / 100000000
+                    ).toFixed(
+                        2
+                    )}B https:XXXXXXXXXXXXXXXXXX.com（Shared From uSMART, Download to trade both HK, US & CN stock）`
                 ]),
                 pageUrl: `www.baidu.com`,
                 shortUrl: `www.baidu.com`,
