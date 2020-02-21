087d50a5f52b5effaabc4415aeb83a669116e6dd
azu <hzxfjmc@163.com>
2020-02-21 19:55:04 +0800

    分享增加overseaPageUrl参数

--- src/pages/fund/index/views/fund-order-list/index.vue
+++ src/pages/fund/index/views/fund-order-list/index.vue
@@ -264,6 +264,7 @@ export default {
                     ]),
                     pageUrl: `${window.location.origin}/${shortPageUrl.url}`,
                     shortUrl: `${this.$appOrigin}/${shortUrl.url}`,
+                    overseaPageUrl: `${this.$appOrigin}/${shortUrl.url}`,
                     thumbUrl: `${window.location.origin}/webapp/marketing/images/mgmChSharev2.png`
                 })
                 this.$toast('分享成功')
