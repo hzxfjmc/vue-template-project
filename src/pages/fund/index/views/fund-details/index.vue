6b32ce7e6cf98967f1aee9859b256767115c9d85
azu <hzxfjmc@163.com>
2020-02-25 15:13:24 +0800

    修改文案

--- src/pages/fund/index/views/fund-details/index.vue
+++ src/pages/fund/index/views/fund-details/index.vue
@@ -681,7 +681,8 @@ export default {
                         this.subscribeButtonShow = false
                     }
                     this.applyAfter = this.$t([
-                        `申购后，好友参与[同行优惠]，可享申购费低至${this.discount}折`,
+                        `申购后，好友参与[同行优惠]，可享申购费低至${this
+                            .discount / 10}折`,
                         `認購後，好友參與「同行優惠」，可享認購費低至${this
                             .discount / 10}折`,
                         `Share with friends, up to ${100 -
