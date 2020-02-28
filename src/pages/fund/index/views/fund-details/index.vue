7e2f130cbbd208a2d0501d2647686c537080040c
azu <hzxfjmc@163.com>
2020-02-28 10:53:25 +0800

    fixed bug

--- src/pages/fund/index/views/fund-details/index.vue
+++ src/pages/fund/index/views/fund-details/index.vue
@@ -1038,7 +1038,7 @@ export default {
                 ) {
                     if (
                         this.fundHeaderInfoVO.derivativeType === 2 ||
-                        this.fundHeaderInfoVO.derivativeType === 2
+                        this.fundHeaderInfoVO.derivativeType === 3
                     ) {
                         return this.$router.push({
                             path: '/risk-appropriate-result',
