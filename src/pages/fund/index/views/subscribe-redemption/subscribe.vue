64c6f9ea04f5711770eb5b9e8e711e6eaa843ea4
azu <hzxfjmc@163.com>
2020-03-13 15:59:34 +0800

    fixed bug

--- src/pages/fund/index/views/subscribe-redemption/subscribe.vue
+++ src/pages/fund/index/views/subscribe-redemption/subscribe.vue
@@ -18,7 +18,7 @@
                             p {{money}}
                             input(
                                 v-model="purchaseAmount" 
-                                type="text"
+                                type="number"
                                 :disabled="disabledInput"
                                 @input="changeNumber"
                                 :placeHolder="`${initialInvestAmount}${currency.type == 1 ? $t('usd') : $t('hkd') }${$t('buyMoneyPlaceHolder')} `" )
