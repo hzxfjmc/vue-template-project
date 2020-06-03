c05a1b8e11cfafc3c9d072b84216e8ba155cf635
linjiajun <linjiajun@youxin.com>
2020-06-03 19:04:38 +0800

    feat: 基金详情分析数据

---
+++ src/pages/fund/index/views/fund-details/components/analyze-data/index.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+    .fund-analyze
+        .fund-analyze__header
+            
+        .fund-analyze__content
+</template>
+<script>
+/**
+ * @description 基金详情页分析数据模块
+ * @author Aaron Lam
+ * @date 2020/06/03
+ */
+export default {
+    i18n: {},
+    components: {},
+    props: {},
+    computed: {},
+    data() {
+        return {}
+    },
+    methods: {},
+    created() {}
+}
+</script>
+<style lang="scss" scoped>
+.fund-analyze__header {
+    display: flex;
+}
+.fund-analyze__content {
+    display: flex;
+}
+</style>
