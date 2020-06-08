9886ec1daa6a5dfb37f6978dc740c535ae8d6f64
linjiajun <linjiajun@youxin.com>
2020-06-08 16:38:48 +0800

    feat: 基金分析数据

--- src/pages/fund/index/views/fund-details/components/analyze-data/column.vue
+++ src/pages/fund/index/views/fund-details/components/analyze-data/column.vue
@@ -30,7 +30,7 @@ export default {
     components: {},
     props: {},
     computed: {
-        ...mapGetters(['stockColorType'])
+        ...mapGetters(['stockColorTypeClass'])
     },
     data() {
         return {}
