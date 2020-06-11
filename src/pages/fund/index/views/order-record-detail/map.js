ceaab510e25e270048a633f93d75f7aba203cbb4
yangchao <yangchao08@youxin.com>
2020-06-11 15:54:31 +0800

    訂單詳情文案修改

---
+++ src/pages/fund/index/views/order-record-detail/map.js
@@ -0,0 +1,23 @@
+export const TRADE_TYPE = {
+    // 申购
+    SUBSCRIBE: 1,
+    // 赎回
+    REDEMPTION: 2,
+    // 其他
+    OTHER: 3,
+    // 分红
+    DIVIDEND: 4
+}
+
+export const ORDER_STATUS = {
+    // 确认中
+    CONFIRMING: 1,
+    // 交易中
+    DEALING: 2,
+    // 交易取消
+    CANCEL: 3,
+    // 交易失败
+    FAILED: 4,
+    // 交易成功
+    SUCCEED: 5
+}
