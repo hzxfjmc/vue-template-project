8c790fab2aae98cd29c5d8ed5e6534e0a269cd2f
xiejun <xiejun_ncu@qq.com>
2020-07-02 14:46:09 +0800

    fix

--- src/pages/fund/index/views/fund-index/fund-article.vue
+++ src/pages/fund/index/views/fund-index/fund-article.vue
@@ -73,7 +73,7 @@ export default {
                     })
                 } else {
                     data = await hkauthorfeature({
-                        author_id: 37,
+                        author_id: 1001,
                         newsid: 0,
                         page_size: 3
                     })
