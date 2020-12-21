e85209eb1f37323b722d4e0d08eadbfcc9c2c92e
linjunfu <linjunfu@youxin.com>
2020-12-21 10:37:15 +0800

    feat：更新签名权限枚举

--- src/utils/common/global-enum.js
+++ src/utils/common/global-enum.js
@@ -10,9 +10,9 @@ export const enumMarketName = {
     2: 'us'
 }
 export const AUTH_CODE = {
-    MARGIN: 4, // 孖展签名权限
-    PI_SIGN: 5, // PI 签名权限
-    FUND_SIGN: 6, // 基金签名权限
-    INTRADAY: 7, // 日内融签名权限
-    STOCK_OPTION: 8 // 期权签名权限
+    FUND_SIGN: 5, // 基金签名权限
+    MARGIN: 6, // 孖展签名权限
+    PI_SIGN: 7, // PI 签名权限
+    INTRADAY: 8, // 日内融签名权限
+    STOCK_OPTION: 9 // 期权签名权限
 }
