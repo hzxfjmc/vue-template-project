fb7402ca9131cac6fc0e453f9451089d449ffa66
linjiajun <linjiajun@youxin.com>
2021-03-03 11:09:23 +0800

    reverse newsupload

--- src/service/news-upload.js
+++ src/service/news-upload.js
@@ -1,6 +1,6 @@
 import httpRequest from '@/utils/http-request'
 import { API_BASE_URL } from '@/utils/DOMAIN.js'
-const axios = new httpRequest(API_BASE_URL.ZX)
+const axios = new httpRequest(API_BASE_URL.HZ)
 
 // 错误日志上报
-export const logjson = data => axios.post('/applog-upload/api/v1/logjson', data)
+export const logjson = data => axios.post('/news-upload/api/v1/logjson', data)
